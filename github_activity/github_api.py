0a95d452881e7a88ddc344d8a636491ae56d1880
CxYeat <aronhandan2008@gmail.com>
2025-07-13 16:17:16 +0200

    Add parse_data() and format_date() functions with initial implementation

--- github_activity/github_api.py
+++ github_activity/github_api.py
@@ -1,5 +1,6 @@
 import json
 import http.client
+from datetime import datetime
 
 def get_user_activity(username : str):
     host = "api.github.com"
@@ -14,4 +15,27 @@ def get_user_activity(username : str):
 
     data = response.read().decode('utf-8')
     events = json.loads(data)
+    #print(json.dumps(events, indent=2))
     return events
+
+def parse_data(events : list):
+    for event in events:
+        event_type = event.get("type")
+        event_repo = event.get("repo").get("name")
+        event_payload = event.get("payload")
+        event_created_at = event.get("created_at")
+
+        if event_type == "WatchEvent":
+            print(f"Starred {event_repo} at {format_date(event_created_at)}")
+        elif event_type == "PushEvent":
+            commits_size = event_payload.get("size")
+            print(f"Pushed {commits_size} to {event_repo}")
+        elif event_type == "ReleaseEvent":
+            pass
+
+def format_date(iso_str : str, output_format : str = "%d.%m.%Y %H:%M"):
+    dt = datetime.strptime(iso_str, "%Y-%m-%dT%H:%M:%SZ")
+    return dt.strftime(output_format)
+
+
+
