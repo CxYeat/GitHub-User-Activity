8f563dd81fe204c1d6d2f3fb837a34cf7df21da4
CxYeat <aronhandan2008@gmail.com>
2025-07-13 03:34:55 +0200

    Rename and restructure modules for clearer separation of concerns

---
+++ github_activity/github_api.py
@@ -0,0 +1,5 @@
+def start_screen():
+    print("Welcome to the GitHub CLI! Every command starts with \"github-activity\"."
+          " For more Information, please use the -h parameter!")
+    github_user = input("Enter a GitHub username: ")
+
